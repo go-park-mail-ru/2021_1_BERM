172f87e8cc234f82b282888ccbd16c5d5b1bddcc
AntonMamedov <tokha.mamiedov@gmail.com>
2021-03-09 18:53:48 +0300

    sadasd

--- apiserver/cors.go
+++ apiserver/cors.go
@@ -1,11 +1,18 @@
 package apiserver
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 
 
 func setupSimpleResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", req.Header.Values("Origin")[0])
+	print("1: ")
+	fmt.Println(req.Header.Values("Origin"))
+	print("2: ")
+	fmt.Println(req.Header.Values("origin"))
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	if cookie := req.Cookies(); len(cookie) != 0 {
 		(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 	}
