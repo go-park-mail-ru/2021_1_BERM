c34309a6a6a28242fe050eb42423cf52d9647fbb
Reutskiy Oleg <astlok@yandex.ru>
2021-04-07 16:14:06 +0300

    bugs fixed

--- schema.sql
+++ schema.sql
@@ -5,6 +5,8 @@ drop table if exists orders cascade;
 drop table if exists order_specializes cascade;
 drop table if exists user_reviews cascade;
 drop table if exists vacancy cascade;
+drop table if exists responses cascade;
+
 CREATE TABLE users
 (
     id           SERIAL PRIMARY KEY NOT NULL,
@@ -38,7 +40,7 @@ CREATE TABLE orders
 (
     id          SERIAL PRIMARY KEY NOT NULL,
     customer_id INTEGER            NOT NULL,
-    executor_id INTEGER DEFAULT 0,
+    executor_id INTEGER,
     order_name  VARCHAR            NOT NULL,
     category    VARCHAR            NOT NULL,
     budget      INTEGER            NOT NULL,
@@ -48,11 +50,11 @@ CREATE TABLE orders
 
 CREATE TABLE vacancy
 (
-    id             SERIAL       PRIMARY KEY NOT NULL,
-    category       VARCHAR      NOT NULL,
-    vacancy_name   VARCHAR      NOT NULL,
-    description    VARCHAR      NOT NULL,
-    salary         INTEGER      NOT NULL
+    id           SERIAL PRIMARY KEY NOT NULL,
+    category     VARCHAR            NOT NULL,
+    vacancy_name VARCHAR            NOT NULL,
+    description  VARCHAR            NOT NULL,
+    salary       INTEGER            NOT NULL
 );
 
 
@@ -70,6 +72,24 @@ CREATE TABLE user_reviews
         REFERENCES orders (id)
 );
 
+CREATE TABLE responses
+(
+    id            SERIAL PRIMARY KEY NOT NULL,
+    order_id      INTEGER            NOT NULL,
+    user_id       INTEGER            NOT NULL,
+    rate          INTEGER            NOT NULL,
+    user_login VARCHAR            NOT NULL,
+    user_img      VARCHAR DEFAULT '',
+    FOREIGN KEY (user_id)
+        REFERENCES users (id),
+    FOREIGN KEY (order_id)
+        REFERENCES orders (id),
+    FOREIGN KEY (user_login)
+        REFERENCES users (login),
+    FOREIGN KEY (user_img)
+        REFERENCES users (img)
+);
+
 
 -- SELECT users.*, array_agg(specialize_name) AS specializes from users
 -- INNER JOIN user_specializes ON users.id = user_specializes.user_id
@@ -79,5 +99,3 @@ CREATE TABLE user_reviews
 
 -- SELECT * from orders
 -- WHERE id = 1
-
-
