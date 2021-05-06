d919d00c5aec87e33d3aae5c171458f962dc25e6
astlok <64102363+astlok@users.noreply.github.com>
2021-05-06 18:11:24 +0300

    fix context test

--- authservice/internal/app/profile/handlers/httphandler_test.go
+++ authservice/internal/app/profile/handlers/httphandler_test.go
@@ -22,9 +22,7 @@ func TestRegistrationProfile(t *testing.T) {
 	metric.New()
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	newUSer := models.NewUser{
-		ID : 1,
-	}
+	newUSer := models.NewUser{}
 	body, _ := json.Marshal(newUSer)
 	req, err := http.NewRequest("POST", "/profile", bytes.NewBuffer(body))
 	ctx := req.Context()
@@ -37,12 +35,12 @@ func TestRegistrationProfile(t *testing.T) {
 	}
 
 	profileUseCaseMock := profileMock.NewMockUseCase(ctrl)
-	profileUseCaseMock.EXPECT().Create(newUSer, ctx).Times(1).Return(&models.UserBasicInfo{
+	profileUseCaseMock.EXPECT().Create(newUSer, context.Background()).Times(1).Return(&models.UserBasicInfo{
 		ID : 1,
 		Executor: true,
 	}, nil)
 	sessionUseCaseMock := sessionMock.NewMockUseCase(ctrl)
-	sessionUseCaseMock.EXPECT().Create(uint64(1), true, ctx).Times(1).Return(&models.Session{}, nil)
+	sessionUseCaseMock.EXPECT().Create(uint64(1), true, context.Background()).Times(1).Return(&models.Session{}, nil)
 
 	handle := New(sessionUseCaseMock, profileUseCaseMock)
 
@@ -52,11 +50,10 @@ func TestRegistrationProfile(t *testing.T) {
 
 	handler.ServeHTTP(recorder, req)
 
-	if status := recorder.Code; status != http.StatusOK {
+	if status := recorder.Code; status != http.StatusAccepted {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusCreated)
 	}
-	//metric.D
-
+	metric.Destroy()
 }
 
